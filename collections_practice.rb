3fd83b0dd4eddb89884be56dece3346eff032d03
Robert Tabares <robtabares92@gmail.com>
2018-05-31 16:51:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,7 @@ def count_elements(arr)
   arr.each do | element |
     counter = element[:counter] = 0
     if output_arr.include?(element) == false
-      counter++
+      counter += 1
       output_arr << element
     end
   end
