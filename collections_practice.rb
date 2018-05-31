2308b7169543a29f1ced3d1243cbbaf6e3c4d6a8
Robert Tabares <robtabares92@gmail.com>
2018-05-31 16:04:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,10 +50,10 @@ def count_elements(arr)
     i+=1
     element[:count] = 0
     if !output.include?(element)
-      element[:count] + 1
       output << element
+
     elsif output.include?(element)
-      element[:count] + 1
+      element[:count] +=1
     end
   end
   output
