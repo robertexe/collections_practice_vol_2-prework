5530deee2734315e79732374dfc572d516206de7
Robert Tabares <robtabares92@gmail.com>
2018-05-31 16:59:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,7 +50,7 @@ def count_elements(arr)
     if output_arr.include?(element) == false
       output_arr << element
   elsif output_arr.include?(element)
-      output_arr.map! do |el|
+      output_arr.map do |el|
         el[:count] + 1
       end
     end
