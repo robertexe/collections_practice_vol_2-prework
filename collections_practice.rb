be04a66e427d2c2d64a41f7d648e3998a7f0f293
Robert Tabares <robtabares92@gmail.com>
2018-05-31 17:23:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,7 +62,7 @@ def merge_data(keys, data)
   data.each do |k, hash|
     puts k
     if k == keys[:first_name]
-      k[:first_name] = keys[:first_name]
+    return 'hello'
     end
   end
   data
