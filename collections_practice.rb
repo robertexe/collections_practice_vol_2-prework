8011c8ccd3b342fead88a12a834f4923d02605b9
Robert Tabares <robtabares92@gmail.com>
2018-05-31 15:22:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,15 +4,24 @@ def begins_with_r(tools)
   }
 end
 
+# def contain_a(arr)
+#   output_arr = [];
+#   arr.each do |element|
+#     word = element.split('')
+#      if word.any? {
+#       |letter| letter == 'a' or letter == 'A'
+#     } == true
+#       output_arr << word.join('')
+#     end
+#   end
+#   return output_arr
+# end
+
 def contain_a(arr)
-  output_arr = [];
-  arr.each do |element|
-    word = element.split('')
-     if word.any? {
-      |letter| letter == 'a' or letter == 'A'
-    } == true
-      output_arr << word.join('')
-    end
+  output_arr = []
+  arr.collect do |element|
+    if element.include?("a")
+      output_arr << element
   end
-  return output_arr
+  return output_arr 
 end
